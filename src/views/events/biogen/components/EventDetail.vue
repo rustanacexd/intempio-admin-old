6116fad5217f45210fab2e7b9960316cf4525bdf
Rustan Corpuz <rustanacexd@gmail.com>
2018-04-17 23:09:03 +0800

    css improvements

--- src/views/events/biogen/components/EventDetail.vue
+++ src/views/events/biogen/components/EventDetail.vue
@@ -1,200 +1,219 @@
 <template>
-  <div class="createPost-container">
-    <div class="createPost-main-container">
+  <div class="form-container">
+    <div class="form-main-container">
       <el-form ref="form" :model="form" :rules="rules" size="small" class="form-container">
-        <el-row :gutter="20">
-          <el-col :lg="8">
-            <el-form-item prop="requestor_name">
-              <el-input v-model="form.requestor_name" placeholder="Requestor Name (required)"/>
-            </el-form-item>
-          </el-col>
-          <el-col :lg="8">
-            <el-form-item prop="phone">
-              <el-input v-model="form.phone" placeholder="Requestor Phone (required)"/>
-            </el-form-item>
-          </el-col>
-          <el-col :lg="8">
-            <el-form-item prop="email">
-              <el-input v-model="form.email" placeholder="Requestor Email (required)"/>
-            </el-form-item>
-          </el-col>
-        </el-row>
-
-        <el-row>
-          <el-col :span="12">
-            <p class="form--label">Event Details</p>
-          </el-col>
-        </el-row>
-
-        <el-row :gutter="20">
-          <el-col :lg="10">
-            <el-form-item prop="name">
-              <el-input v-model="form.name" placeholder="Event Name (required)"/>
-            </el-form-item>
-          </el-col>
-
-          <el-col :lg="5">
-            <el-form-item prop="date">
-              <el-date-picker type="date" placeholder="Event Date (required)" v-model="form.date" style="width: 100%;"
-                              value-format="yyyy-MM-dd"/>
-            </el-form-item>
-          </el-col>
-
-          <el-col :lg="6">
-            <el-form-item prop="time">
-              <el-time-select v-model="form.time" placeholder="Event Time (required)"
-                              :picker-options="timePickerOptions"/>
-            </el-form-item>
-          </el-col>
-
-          <el-col :lg="3">
-            <el-form-item prop="period">
-              <el-radio-group v-model="form.period">
-                <el-radio-button label="am">AM</el-radio-button>
-                <el-radio-button label="pm">PM</el-radio-button>
-              </el-radio-group>
-            </el-form-item>
-          </el-col>
-        </el-row>
-
-        <el-row>
-          <el-col :lg="12">
+        <el-tabs tab-position="left">
+          <el-tab-pane label="Event Details">
             <el-row>
-              <el-col :lg="12">
-                <p class="form--label">Event Duration</p>
+              <el-col :span="12">
+                <h5 class="form--label">Requestor Info</h5>
+              </el-col>
+            </el-row>
+            <el-row :gutter="20">
+              <el-col :lg="8">
+                <el-form-item prop="requestor_name">
+                  <el-input v-model="form.requestor_name" placeholder="Requestor Name (required)"/>
+                </el-form-item>
+              </el-col>
+              <el-col :lg="8">
+                <el-form-item prop="phone">
+                  <el-input v-model="form.phone" placeholder="Requestor Phone (required)"/>
+                </el-form-item>
+              </el-col>
+              <el-col :lg="8">
+                <el-form-item prop="email">
+                  <el-input v-model="form.email" placeholder="Requestor Email (required)"/>
+                </el-form-item>
               </el-col>
             </el-row>
 
-            <el-form-item prop="duration">
-              <el-radio-group v-model="form.duration" size="small">
-                <el-radio border :label="60">1 hour</el-radio>
-                <el-radio :label="90" border>1 1/2 hours</el-radio>
-                <el-radio :label="120" border>2 hours</el-radio>
-                <el-radio :label="240" border>half day</el-radio>
-                <el-radio :label="480" border>all day</el-radio>
-              </el-radio-group>
-            </el-form-item>
-          </el-col>
-
-          <el-col :lg="12">
             <el-row>
-              <el-col :lg="12">
-                <p class="form--label">Timezone</p>
+              <el-col :span="12">
+                <h5 class="form--label">Event Details</h5>
               </el-col>
             </el-row>
 
-            <el-form-item prop="timezone">
-              <el-select v-model="form.timezone" placeholder="(required)">
-                <el-option label="EST" value="EST"/>
-                <el-option label="EU/Basel" value="EU/Basel"/>
-                <el-option label="EU/Dublin" value="EU/Dublin"/>
-                <el-option label="US/NY" value="US/NY"/>
-              </el-select>
-            </el-form-item>
+            <el-row :gutter="20">
+              <el-col :lg="10">
+                <el-form-item prop="name">
+                  <el-input v-model="form.name" placeholder="Event Name (required)"/>
+                </el-form-item>
+              </el-col>
 
-          </el-col>
-        </el-row>
+              <el-col :lg="5">
+                <el-form-item prop="date">
+                  <el-date-picker type="date" placeholder="Event Date (required)" v-model="form.date"
+                                  style="width: 100%;"
+                                  value-format="yyyy-MM-dd"/>
+                </el-form-item>
+              </el-col>
 
-        <el-row :gutter="20">
-          <el-col :lg="7">
-            <el-form-item label="No. of participants" prop="participants_count">
-              <el-input-number v-model="form.participants_count" controls-position="right" :min="1"/>
-              <el-tooltip style="margin-left: 5px" class="item" effect="dark"
-                          content="Expected number of participants planned for this event?" placement="top">
-                <i class="el-icon-question"></i>
-              </el-tooltip>
-            </el-form-item>
+              <el-col :lg="6">
+                <el-form-item prop="time">
+                  <el-time-select v-model="form.time" placeholder="Event Time (required)"
+                                  :picker-options="timePickerOptions"/>
+                </el-form-item>
+              </el-col>
 
-          </el-col>
-          <el-col :lg="7">
-            <el-form-item label="No. of presenters" prop="presenters_count">
-              <el-input-number v-model="form.presenters_count" controls-position="right" :min="1"/>
-              <el-tooltip style="margin-left: 5px" class="item" effect="dark"
-                          content="Number of presenters planned for the event" placement="top">
-                <i class="el-icon-question"></i>
-              </el-tooltip>
-            </el-form-item>
-          </el-col>
+              <el-col :lg="3">
+                <el-form-item prop="period">
+                  <el-radio-group v-model="form.period">
+                    <el-radio-button label="am">AM</el-radio-button>
+                    <el-radio-button label="pm">PM</el-radio-button>
+                  </el-radio-group>
+                </el-form-item>
+              </el-col>
+            </el-row>
 
-          <el-col :lg="4">
-            <el-form-item prop="ms_sma">
-              <el-radio-group v-model="form.ms_sma">
-                <el-radio-button label="MS">MS</el-radio-button>
-                <el-radio-button label="MSA">SMA</el-radio-button>
-              </el-radio-group>
-            </el-form-item>
-          </el-col>
-          <el-col :lg="4">
-            <el-form-item prop="eod_webcast">
-              <el-radio-group v-model="form.eod_webcast">
-                <el-radio-button label="EOD">EOD</el-radio-button>
-                <el-radio-button label="Webcast">Webcast</el-radio-button>
-              </el-radio-group>
-            </el-form-item>
-          </el-col>
+            <el-row>
+              <el-col :lg="12">
+                <el-row>
+                  <el-col :lg="12">
+                    <h5 class="form--label">Event Duration</h5>
+                  </el-col>
+                </el-row>
 
-        </el-row>
+                <el-form-item prop="duration">
+                  <el-radio-group v-model="form.duration" size="small">
+                    <el-radio border :label="60">1 hour</el-radio>
+                    <el-radio :label="90" border>1 1/2 hours</el-radio>
+                    <el-radio :label="120" border>2 hours</el-radio>
+                    <el-radio :label="240" border>half day</el-radio>
+                    <el-radio :label="480" border>all day</el-radio>
+                  </el-radio-group>
+                </el-form-item>
+              </el-col>
 
-        <el-row :gutter="20">
-          <el-col :lg="8">
-            <el-form-item prop="slide_deck_name">
-              <el-input v-model="form.slide_deck_name" placeholder="Slide Deck Name"/>
-            </el-form-item>
-          </el-col>
-          <el-col :lg="8">
-            <el-form-item prop="slide_deck_id">
-              <el-input v-model="form.slide_deck_id" placeholder="Slide Deck ID"/>
-            </el-form-item>
-          </el-col>
-          <el-col :lg="8">
-            <el-form-item prop="program_meeting_id">
-              <el-input v-model="form.program_meeting_id" placeholder="Program Meeting ID"/>
-            </el-form-item>
-          </el-col>
-        </el-row>
+              <el-col :lg="12">
+                <el-row>
+                  <el-col :lg="12">
+                    <h5 class="form--label">Timezone</h5>
+                  </el-col>
+                </el-row>
 
-        <el-row>
-          <el-col :span="12">
-            <p class="form--label">Presenters</p>
-          </el-col>
-        </el-row>
+                <el-form-item prop="timezone">
+                  <el-select v-model="form.timezone" placeholder="(required)">
+                    <el-option label="EST" value="EST"/>
+                    <el-option label="EU/Basel" value="EU/Basel"/>
+                    <el-option label="EU/Dublin" value="EU/Dublin"/>
+                    <el-option label="US/NY" value="US/NY"/>
+                  </el-select>
+                </el-form-item>
 
-        <el-row v-for="(presenter, index) in form.presenters" :key="index" :gutter="20">
-          <el-col :lg="10">
-            <el-form-item :prop="'presenters.' + index + '.name'">
-              <el-input v-model="presenter.name" placeholder="Presenter Name"/>
-            </el-form-item>
-          </el-col>
-          <el-col :lg="10">
-            <el-form-item :rules="[{ type: 'email', message: 'Please input correct email address', trigger: 'change' }]"
-                          :prop="'presenters.' + index + '.email'">
-              <el-input v-model="presenter.email" placeholder="Presenter Email"/>
-            </el-form-item>
-          </el-col>
+              </el-col>
+            </el-row>
 
-          <el-col :span="2">
-            <el-button circle @click.prevent="removePresenter(presenter)" icon="el-icon-delete" size="small"
-                       style="margin-bottom: 10px"/>
-          </el-col>
-        </el-row>
+            <el-row :gutter="20">
+              <el-col :lg="7">
+                <el-form-item label="No. of participants" prop="participants_count">
+                  <el-input-number v-model="form.participants_count" controls-position="right" :min="1"/>
+                  <el-tooltip style="margin-left: 5px" class="item" effect="dark"
+                              content="Expected number of participants planned for this event?" placement="top">
+                    <i class="el-icon-question"></i>
+                  </el-tooltip>
+                </el-form-item>
 
-        <p>
-          <el-button type="primary" icon="el-icon-plus" @click="addPresenter" size="small">
-            add presenter
-          </el-button>
-        </p>
+              </el-col>
+              <el-col :lg="7">
+                <el-form-item label="No. of presenters" prop="presenters_count">
+                  <el-input-number v-model="form.presenters_count" controls-position="right" :min="1"/>
+                  <el-tooltip style="margin-left: 5px" class="item" effect="dark"
+                              content="Number of presenters planned for the event" placement="top">
+                    <i class="el-icon-question"></i>
+                  </el-tooltip>
+                </el-form-item>
+              </el-col>
 
-        <el-row>
-          <el-col :lg="16">
-            <el-form-item label="Additional Notes" prop="notes">
-              <el-input type="textarea" :rows="4" placeholder="add information not found on the fields above"
-                        v-model="form.notes"/>
-            </el-form-item>
-          </el-col>
-        </el-row>
+              <el-col :lg="4">
+                <el-form-item prop="ms_sma">
+                  <el-radio-group v-model="form.ms_sma">
+                    <el-radio-button label="MS">MS</el-radio-button>
+                    <el-radio-button label="MSA">SMA</el-radio-button>
+                  </el-radio-group>
+                </el-form-item>
+              </el-col>
+              <el-col :lg="4">
+                <el-form-item prop="eod_webcast">
+                  <el-radio-group v-model="form.eod_webcast">
+                    <el-radio-button label="EOD">EOD</el-radio-button>
+                    <el-radio-button label="Webcast">Webcast</el-radio-button>
+                  </el-radio-group>
+                </el-form-item>
+              </el-col>
 
-        <el-form-item>
-          <el-button type="primary " @click="submitForm" icon="el-icon-upload2">
+            </el-row>
+
+            <el-row :gutter="20">
+              <el-col :lg="8">
+                <el-form-item prop="slide_deck_name">
+                  <el-input v-model="form.slide_deck_name" placeholder="Slide Deck Name"/>
+                </el-form-item>
+              </el-col>
+              <el-col :lg="8">
+                <el-form-item prop="slide_deck_id">
+                  <el-input v-model="form.slide_deck_id" placeholder="Slide Deck ID"/>
+                </el-form-item>
+              </el-col>
+              <el-col :lg="8">
+                <el-form-item prop="program_meeting_id">
+                  <el-input v-model="form.program_meeting_id" placeholder="Program Meeting ID"/>
+                </el-form-item>
+              </el-col>
+            </el-row>
+
+            <el-row>
+              <el-col :span="12">
+                <h5 class="form--label">Presenters</h5>
+              </el-col>
+            </el-row>
+
+            <el-row v-for="(presenter, index) in form.presenters" :key="index" :gutter="20">
+              <el-col :lg="10">
+                <el-form-item :prop="'presenters.' + index + '.name'">
+                  <el-input v-model="presenter.name" placeholder="Presenter Name"/>
+                </el-form-item>
+              </el-col>
+              <el-col :lg="10">
+                <el-form-item
+                  :rules="[{ type: 'email', message: 'Please input correct email address', trigger: 'change' }]"
+                  :prop="'presenters.' + index + '.email'">
+                  <el-input v-model="presenter.email" placeholder="Presenter Email"/>
+                </el-form-item>
+              </el-col>
+
+              <el-col :span="2">
+                <el-button circle @click.prevent="removePresenter(presenter)" icon="el-icon-delete" size="small"
+                           style="margin-bottom: 10px"/>
+              </el-col>
+            </el-row>
+
+            <p>
+              <el-button type="primary" icon="el-icon-plus" @click="addPresenter" size="small">
+                add presenter
+              </el-button>
+            </p>
+
+            <el-row>
+              <el-col :span="12">
+                <h5 class="form--label">Additional Notes</h5>
+              </el-col>
+            </el-row>
+
+            <el-row>
+              <el-col :lg="16">
+                <el-form-item prop="notes">
+                  <el-input type="textarea" :rows="4" placeholder="add information not found on the fields above"
+                            v-model="form.notes"/>
+                </el-form-item>
+              </el-col>
+            </el-row>
+          </el-tab-pane>
+
+          <el-tab-pane label="Project Details">Project</el-tab-pane>
+          <el-tab-pane label="Contact(s)">Contact(s)</el-tab-pane>
+        </el-tabs>
+        <el-form-item class="page-component-up">
+          <el-button type="primary " @click="submitForm" icon="el-icon-upload2" size="large">
             {{ isEdit ? 'Update' : 'Submit'}} Event
           </el-button>
         </el-form-item>
@@ -344,48 +363,3 @@
     }
   }
 </script>
-
-<style rel="stylesheet/scss" lang="scss" scoped>
-  @import "src/styles/mixin.scss";
-
-  .title-prompt {
-    position: absolute;
-    right: 0px;
-    font-size: 12px;
-    top: 10px;
-    color: #ff4949;
-  }
-
-  .createPost-container {
-    position: relative;
-    .createPost-main-container {
-      padding: 40px 45px 20px 50px;
-      .postInfo-container {
-        position: relative;
-        @include clearfix;
-        margin-bottom: 10px;
-        .postInfo-container-item {
-          float: left;
-        }
-      }
-      .editor-container {
-        min-height: 500px;
-        margin: 0 0 30px;
-        .editor-upload-btn-container {
-          text-align: right;
-          margin-right: 10px;
-          .editor-upload-btn {
-            display: inline-block;
-          }
-        }
-      }
-    }
-    .word-counter {
-      width: 40px;
-      position: absolute;
-      right: -10px;
-      top: 0px;
-    }
-  }
-</style>
-
