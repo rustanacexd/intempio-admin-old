357bb47be5abaac967833d4f53a505be9799c80f
Rustan Corpuz <rustanacexd@gmail.com>
2018-04-23 14:05:21 +0800

    setup base_api url for staging

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://intempio-api-prod.herokuapp.com/api/v1"',
-  // BASE_API: '"https://intempio-api-staging.herokuapp.com/api/v1"',
+  // BASE_API: '"https://intempio-api-prod.herokuapp.com/api/v1"',
+  BASE_API: '"https://intempio-api-staging.herokuapp.com/api/v1"',
 }
