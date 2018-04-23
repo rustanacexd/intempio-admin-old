c1884cf6eca1836bbf1157bb1a41c19ebe663c75
Rustan Corpuz <rustanacexd@gmail.com>
2018-04-24 04:38:26 +0800

    login fix

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,6 +1,5 @@
 export function isvalidUsername(str) {
-  const valid_map = ['admin', 'editor', 'rustan']
-  return valid_map.indexOf(str.trim()) >= 0
+  return true
 }
 
 export function validateURL(textval) {
